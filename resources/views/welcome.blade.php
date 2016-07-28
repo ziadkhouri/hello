8124f3cc71cc40ad2b29de22d0c933f30645349d
vagrant <vagrant@homestead>
2016-07-28 09:24:38 +0000

    Tidy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,9 @@
                         <li>The endpoint is "/register"
                         <li>The device token has all whitespace removed before being saved to the database. This is done in the AuthController.
                         <li>The laraval Queues API is used to queue push notifications so they do not "block" and impact the user experience
+                        <li>The Notification library is contained in App/Notifications
                         <li>Notifications are stored in the database
+                        <li>New notification types can be added by subclassing NotificationMessage. Some examples are included.
                         <li>Apple Push Notifications are implemented using https://github.com/davibennun/laravel-push-notification
                         <li>The certificate I use is self-signed, and will be rejected by Apple.
                         <li>I used the laravel Authentication API as a shortcut to create the "new user" functionality. This is kind of overkill, and I could remove the redundant functionality if required.
